2537c892f7af8769e9e022f53d7ace2eb320ab2c
StyearX <nazzz9442@gmail.com>
2026-05-27 17:53:07 +0800

    Add: Google Material Icons And Hero Icons

--- Main-v2.lua
+++ Main-v2.lua
@@ -1,5 +1,3 @@
--- This is a new version with Google Material Icons
-
 local cloneref = (cloneref or clonereference or function(instance)
 	return instance
 end)
@@ -42,6 +40,9 @@ local IconModule = {
 		)(),
 		googlematerial = loadstring(
 			Get("https://raw.githubusercontent.com/StyearX/Icons/refs/heads/main/GoogleMaterialIcons/dist/Icons.lua")
+		)(),
+		hero = loadstring(
+			Get("https://raw.githubusercontent.com/StyearX/Icons/refs/heads/main/hero/dist/Icons.lua")
 		)(),
 	},
 }
@@ -242,97 +243,7 @@ function IconModule.Image(IconConfig)
 	return Icon
 end
 
-return IconModule	end
-
-	for iconName, iconValue in pairs(iconsData) do
-		if type(iconValue) == "number" or (type(iconValue) == "string" and iconValue:match("^rbxassetid://")) then
-			local imageId = iconValue
-			if type(iconValue) == "number" then
-				imageId = "rbxassetid://" .. tostring(iconValue)
-			end
-
-			IconModule.Icons[packName].Icons[iconName] = {
-				Image = imageId,
-				ImageRectSize = Vector2.new(0, 0),
-				ImageRectPosition = Vector2.new(0, 0),
-				Parts = nil,
-			}
-			IconModule.Icons[packName].Spritesheets[imageId] = imageId
-		elseif type(iconValue) == "table" then
-			if iconValue.Image and iconValue.ImageRectSize and iconValue.ImageRectPosition then
-				local imageId = iconValue.Image
-				if type(imageId) == "number" then
-					imageId = "rbxassetid://" .. tostring(imageId)
-				end
-
-				IconModule.Icons[packName].Icons[iconName] = {
-					Image = imageId,
-					ImageRectSize = iconValue.ImageRectSize,
-					ImageRectPosition = iconValue.ImageRectPosition,
-					Parts = iconValue.Parts,
-				}
-
-				if not IconModule.Icons[packName].Spritesheets[imageId] then
-					IconModule.Icons[packName].Spritesheets[imageId] = imageId
-				end
-			else
-				warn("AddIcons: Invalid spritesheet data format for icon '" .. iconName .. "'")
-			end
-		else
-			warn("AddIcons: Unsupported data type for icon '" .. iconName .. "': " .. type(iconValue))
-		end
-	end
-end
-
-function IconModule.SetIconsType(iconType)
-	IconModule.IconsType = iconType
-end
-
-function IconModule.Init(New, IconThemeTag)
-	IconModule.New = New
-	IconModule.IconThemeTag = IconThemeTag
-
-	return IconModule
-end
-
-function IconModule.Icon(Icon, Type, DefaultFormat)
-	DefaultFormat = DefaultFormat ~= false
-	local iconType, iconName = parseIconString(Icon)
-
-	local targetType = iconType or Type or IconModule.IconsType
-	local targetName = iconName
-
-	local iconSet = IconModule.Icons[targetType]
-
-	if iconSet and iconSet.Icons and iconSet.Icons[targetName] then
-		return {
-			iconSet.Spritesheets[tostring(iconSet.Icons[targetName].Image)],
-			iconSet.Icons[targetName],
-		}
-	elseif iconSet and iconSet[targetName] and string.find(iconSet[targetName], "rbxassetid://") then
-		return DefaultFormat
-				and {
-					iconSet[targetName],
-					{ ImageRectSize = Vector2.new(0, 0), ImageRectPosition = Vector2.new(0, 0) },
-				}
-			or iconSet[targetName]
-	end
-	return nil
-end
-
-function IconModule.GetIcon(Icon, Type)
-	return IconModule.Icon(Icon, Type, false)
-end
-
-function IconModule.Icon2(Icon, Type, DefaultFormat)
-	return IconModule.Icon(Icon, Type, true)
-end
-
-function IconModule.Image(IconConfig)
-	local Icon = {
-		Icon = IconConfig.Icon or nil,
-		Type = IconConfig.Type,
-		Colors = IconConfig.Colors or { (IconModule.IconThemeTag or Color3.new(1, 1, 1)), Color3.new(1, 1, 1) },
+return IconModuleor3.new(1, 1, 1) },
 		Size = IconConfig.Size or UDim2.new(0, 24, 0, 24),
 
 		IconFrame = nil,
